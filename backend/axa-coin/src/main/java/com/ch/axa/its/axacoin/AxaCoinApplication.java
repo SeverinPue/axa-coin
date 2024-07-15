cfec67692cffdf19989ca7524a21682584fe4d61
SeverinPue <59147017+SeverinPue@users.noreply.github.com>
2024-07-15 10:23:31 +0200

    first backend commit

---
+++ backend/axa-coin/src/main/java/com/ch/axa/its/axacoin/AxaCoinApplication.java
@@ -0,0 +1,13 @@
+package com.ch.axa.its.axacoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxaCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxaCoinApplication.class, args);
+    }
+
+}
---
+++ backend/axa-coin/src/test/java/com/ch/axa/its/axacoin/AxaCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.axa.its.axacoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxaCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
