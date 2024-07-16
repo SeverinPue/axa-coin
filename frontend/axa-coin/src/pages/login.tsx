1fbb89940ddebc6c00be0e7e6113591436a30267
SeverinPue <59147017+SeverinPue@users.noreply.github.com>
2024-07-16 12:59:13 +0200

    login side create

---
+++ frontend/axa-coin/src/pages/login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/navbar.tsx";
+
+export default function Home() {
+  return (
+    <>
+
+        <form action="">
+
+            <input type="text" id="username" name="username"/>
+            <input type="passwort" id="username" name="username"/>
+        </form>
+    </>
+  );
+}
