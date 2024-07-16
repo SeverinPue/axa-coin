f1161f0b1eba571c86d8280486bfc377deb49206
jay <jay.sellers2000@gmail.com>
2024-07-16 17:15:31 +0200

    styling for button in index.css, completed task component with styling, installed react-icons with npm and implemented in task component

--- frontend/axa-coin/src/components/task.jsx
+++ frontend/axa-coin/src/components/task.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import "./stylesheets/navbar.css";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import "./stylesheets/task.css";
 
 export default function Task({ title, description, endDate, earningPoints }) {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
@@ -10,16 +11,22 @@ export default function Task({ title, description, endDate, earningPoints }) {
 
   return (
     <div className="card">
-      <p className="title">{title}</p>
-      <p className="date">{endDate}</p>
-      <button className="toggle-button" onClick={toggleMenu}>
-        {isMenuVisible ? "Hide Details" : "Show Details"}
-      </button>
-      {isMenuVisible && (
+      <div className="toplevel">
+        <p className="title">{title}</p>
+        <p className="date">{endDate}</p>
+        {isMenuVisible ? (
+          <SlArrowUp className="toggle-button" onClick={toggleMenu} />
+        ) : (
+          <SlArrowDown className="toggle-button" onClick={toggleMenu} />
+        )}
+      </div>
+      {isMenuVisible ? (
         <div className="toggle-menu">
           <p className="description">{description}</p>
           <p className="earningPoints">{earningPoints} Points</p>
         </div>
+      ) : (
+        <div className="toggle-menu"/>
       )}
     </div>
   );
