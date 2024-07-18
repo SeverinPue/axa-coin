e8e6bce4f70132610e164dbbc30753c627b8312f
thlivio <anonym.junk@outlook.com>
2024-07-18 10:48:01 +0200

    UpdateTraineeDto.java

---
+++ backend/axa-coin/src/main/java/com/ch/axa/its/axacoin/Entity/UpdateTraineeDto.java
@@ -0,0 +1,13 @@
+package com.ch.axa.its.axacoin.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateTraineeDto {
+    private String id;
+    private String username;
+    private String trainerId;
+    private String userId;
+}
