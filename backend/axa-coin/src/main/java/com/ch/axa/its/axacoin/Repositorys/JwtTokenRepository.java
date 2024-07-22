c3012872aaa2a1a104fb849436e84d0f5efab13d
thlivio <anonym.junk@outlook.com>
2024-07-22 08:33:05 +0200

    adföjklndfLÖINGÖDJILN

--- backend/axa-coin/src/main/java/com/ch/axa/its/axacoin/Repositorys/JwtTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ch.axa.its.axacoin.Repositorys;
-
-import com.ch.axa.its.axacoin.Entity.JwtToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JwtTokenRepository extends JpaRepository<JwtToken, Long> {
-    JwtToken findByToken(String token);
-}
