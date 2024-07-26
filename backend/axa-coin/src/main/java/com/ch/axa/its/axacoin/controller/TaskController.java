ca049adf7ebcccb5be6257ee3637bdace2e07373
thlivio <anonym.junk@outlook.com>
2024-07-26 13:23:07 +0200

    favicon

--- backend/axa-coin/src/main/java/com/ch/axa/its/axacoin/controller/TaskController.java
+++ backend/axa-coin/src/main/java/com/ch/axa/its/axacoin/controller/TaskController.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @Hidden
 public class TaskController {
 
